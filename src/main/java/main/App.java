1ca080fa552119c0d21bfdb003ea97fee2faa2ec
khizrkshah <khizrkshah@gmail.com>
2021-11-07 16:00:31 +0100

    Added three windows as fxml files for scene builder along with their controllers.

--- src/main/java/main/App.java
+++ src/main/java/main/App.java
@@ -2,7 +2,9 @@ package main;
 
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -13,12 +15,30 @@ public class App extends Application{
 	  public void start(Stage primaryStage) {
 		  
 		    Group group = new Group();
-	        
-	        Scene scene = new Scene(group, 1024, 800);
-	        
-	        primaryStage.setScene(scene);
-	        primaryStage.show();
-	        
+
+		  Stage previewStage = new Stage();
+
+		  try {
+			  FXMLLoader loader = new FXMLLoader(); // FXML for primary stage
+			  Parent root = loader.load(getClass().getResource("/scenes/Editor_Screen.fxml"));
+			  Scene scene = new Scene(root);
+			  primaryStage.setScene(scene);
+			  primaryStage.show();
+			  primaryStage.setTitle("Editor");
+			  primaryStage.setX(50);
+
+			  FXMLLoader anotherLoader = new FXMLLoader() ; // FXML for second stage
+			  Parent anotherRoot = anotherLoader.load(getClass().getResource("/scenes/Preview_Screen.fxml"));
+			  Scene anotherScene = new Scene(anotherRoot);
+			  previewStage.setScene(anotherScene);
+			  previewStage.show();
+			  previewStage.setTitle("Preview");
+
+		  } catch (Exception exc) {
+
+			  exc.printStackTrace();
+
+		  }
 	        
 	    }
 	  
